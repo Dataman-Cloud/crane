d0a38b150e5efcc62a21340ff549be3bdf781a74
upccup <yaoyunsmile@163.com>
2016-07-08 10:25:40 +0800

    init go server framework  gin

---
+++ trunk/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+    r := gin.Default()
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "pong",
+        })
+    })
+	r.Run(":5013")
+}
