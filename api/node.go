80acbc1409be423ad3192d7125c87cd4aae42b9c
upccup <yaoyunsmile@163.com>
2016-07-08 17:41:38 +0800

    add get nodes api

--- api/node.go
+++ api/node.go
@@ -1,11 +1,30 @@
 package api
 
 import (
+	"net/http"
+
+	"github.com/Dataman-Cloud/rolex/dockerclient"
 	"github.com/gin-gonic/gin"
 )
 
-func (api *Api) GetNode(ctx *gin.Context)    {}
 func (api *Api) ListNode(ctx *gin.Context)   {}
 func (api *Api) CreateNode(ctx *gin.Context) {}
 func (api *Api) UpdateNode(ctx *gin.Context) {}
 func (api *Api) DeleteNode(ctx *gin.Context) {}
+=
+func GetNodes(c *gin.Context) {
+	client, err := dockerclient.NewDockerGoClient("http://192.168.59.106:2376")
+	if err != nil {
+		c.JSON(http.StatusServiceUnavailable, err.Error())
+		return
+	}
+
+	opts := dockerclient.NodeListOptions{}
+	nodes, err := client.Nodelist(opts)
+	if err != nil {
+		c.JSON(http.StatusServiceUnavailable, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"code": 0, "data": nodes})
+}
