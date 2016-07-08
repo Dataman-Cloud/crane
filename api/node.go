d5f1cacd398583fa444d260e757916749148388f
kevin xu <cming.xu@gmail.com>
2016-07-08 17:42:01 +0800

    [MOD] api skeleton added

---
+++ api/node.go
@@ -0,0 +1,11 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (api *Api) GetNode(ctx *gin.Context)    {}
+func (api *Api) ListNode(ctx *gin.Context)   {}
+func (api *Api) CreateNode(ctx *gin.Context) {}
+func (api *Api) UpdateNode(ctx *gin.Context) {}
+func (api *Api) DeleteNode(ctx *gin.Context) {}
