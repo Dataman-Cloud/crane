4fc2b5028b90881174fd8929f01c701d5cf9099c
yqguodataman <yqguo@dataman-inc.com>
2016-07-09 10:18:59 +0800

    fix error syntax and add gin log

--- model/service.go
+++ model/service.go
@@ -2,6 +2,7 @@ package model
 
 type Service struct {
 	ID      uint64
+	UserId  uint64 `gorm:"not null;type:varchar(64)"`
 	Name    string `gorm:"not null"`
 	StackId uint64 `gorm:"type:varchar(64)"`
 }
