1f3509cc51c58c24557595fc72f2e33b7d111889
my9074 <313231068@qq.com>
2016-07-11 17:37:36 +0800

    Add network module

---
+++ src/network/create/create.ctrl.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+    angular.module('app.network')
+        .controller('CreateCtrl', CreateCtrl);
+
+    /* @ngInject */
+    function CreateCtrl() {
+        var self = this;
+
+        activate();
+
+        function activate() {
+            ///
+        }
+    }
+})();
---
+++ src/network/services/network-curd-service.js
@@ -0,0 +1,18 @@
+/**
+ * Created by my9074 on 16/3/9.
+ */
+(function () {
+    'use strict';
+    angular.module('app.network')
+        .factory('networkCurd', networkCurd);
+
+
+    /* @ngInject */
+    function networkCurd(nodeBackend) {
+        //////
+        return {
+
+        };
+
+    }
+})();
