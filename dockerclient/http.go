1f327c673a584869a0a2b15cd00b104d6b644b7a
upccup <yaoyunsmile@163.com>
2016-07-13 19:59:24 +0800

    add scale service api

--- dockerclient/http.go
+++ dockerclient/http.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 // execute http get request use default timeout
@@ -50,13 +51,21 @@ func (client *RolexDockerClient) HttpDelete(requestPath string) ([]byte, error) 
 	return ioutil.ReadAll(resp.Body)
 }
 
-func (client *RolexDockerClient) HttpPost(requestPath string, body []byte) ([]byte, error) {
-	req, err := http.NewRequest("POST", client.HttpEndpoint+"/"+requestPath, bytes.NewBuffer(body))
+func (client *RolexDockerClient) HttpPost(requestPath string, query url.Values, body []byte, headers map[string][]string) ([]byte, error) {
+	apiPath := client.getAPIPath(client.HttpEndpoint+"/"+requestPath, query)
+	req, err := http.NewRequest("POST", apiPath, bytes.NewBuffer(body))
+
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	if headers != nil {
+		for k, v := range headers {
+			req.Header[k] = v
+		}
+	}
 
 	resp, err := client.HttpClient.Do(req)
 	if err != nil {
@@ -73,4 +82,47 @@ func (client *RolexDockerClient) HttpPost(requestPath string, body []byte) ([]by
 	defer resp.Body.Close()
 
 	return ioutil.ReadAll(resp.Body)
+}
+
+func (client *RolexDockerClient) HttpPut(requestPath string, query url.Values, body []byte, headers map[string][]string) ([]byte, error) {
+	apiPath := client.getAPIPath(client.HttpEndpoint+"/"+requestPath, query)
+	req, err := http.NewRequest("PUT", apiPath, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	if headers != nil {
+		for k, v := range headers {
+			req.Header[k] = v
+		}
+	}
+
+	resp, err := client.HttpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("http response status code is %d", resp.StatusCode)
+	}
+
+	if resp.Body == nil {
+		return nil, nil
+	}
+	defer resp.Body.Close()
+
+	return ioutil.ReadAll(resp.Body)
+}
+
+// getAPIPath returns the versioned request path to call the api.
+// It appends the query parameters to the path if they are not empty.
+func (client *RolexDockerClient) getAPIPath(apiPath string, query url.Values) string {
+	u := &url.URL{
+		Path: apiPath,
+	}
+	if len(query) > 0 {
+		u.RawQuery = query.Encode()
+	}
+	return u.String()
 }
