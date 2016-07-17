becff37906bf1cd3ef7430067511f1946bd23dc2
my9074 <313231068@qq.com>
2016-07-17 13:03:55 +0800

    Add handle for node

--- src/node/list/list.ctrl.js
+++ src/node/list/list.ctrl.js
@@ -5,15 +5,43 @@
 
 
     /* @ngInject */
-    function NodeListCtrl(nodes) {
+    function NodeListCtrl(nodes, nodeCurd) {
         var self = this;
 
         self.nodes = nodes;
+
+        self.drainNode = drainNode;
+        self.activeNode = activeNode;
+        self.pauseNode = pauseNode;
 
         activate();
 
         function activate() {
             ///
+        }
+
+        function drainNode(nodeId, node) {
+            var data = handleNodeFormat(node);
+            nodeCurd.drainNode(nodeId, data)
+        }
+
+        function activeNode(nodeId, node) {
+            var data = handleNodeFormat(node);
+            nodeCurd.activeNode(nodeId, data)
+        }
+
+        function pauseNode(nodeId, node) {
+            var data = handleNodeFormat(node);
+            nodeCurd.pauseNode(nodeId, data)
+        }
+
+        function handleNodeFormat(node) {
+            var data = {
+                Spec: node.Spec || '',
+                Version: node.Version || ''
+            };
+
+            return data;
         }
     }
 })();
