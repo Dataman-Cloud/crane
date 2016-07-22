23b9040565885b320c7bbb0e4709d759b0e25ece
jfchen <jfchen@dataman-inc.com>
2016-07-22 13:17:27 +0800

    optimize:container chart

--- src/node/container-detail/stats.ctrl.js
+++ src/node/container-detail/stats.ctrl.js
@@ -18,8 +18,7 @@
         function listenStats() {
             stream = stream.Stream('node.containerStats', {node_id:$stateParams.node_id, container_id:$stateParams.container_id});
             stream.addHandler('container-stats', function (event) {
-                self.chartOptions.pushData(event.data);
-                $scope.$apply();
+                self.chartOptions.pushData(event.data, self.cpuChartApi, self.memChartApi, self.networkChartApi);
             });
             stream.start();
             
