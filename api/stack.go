51541bff8c2595e7f95b3a140f0061cea69c876d
YiQin Guo <yqguo@dataman-inc.com>
2016-07-27 19:09:56 +0800

    deploy stack add permission (#140)

--- api/stack.go
+++ api/stack.go
@@ -33,18 +33,34 @@ func (api *Api) CreateStack(ctx *gin.Context) {
 		return
 	}
 
-	var perms map[string]string
-	if ctx.Query("group_id") != "" {
+	if api.Config.FeatureEnabled("account") {
+		if ctx.Query("group_id") == "" {
+			log.Error("invalid group_id")
+			ctx.JSON(http.StatusBadRequest, gin.H{"code": 0, "data": "invalid group_id"})
+			return
+		}
+
 		groupId, err := strconv.ParseUint(ctx.Query("group_id"), 10, 64)
 		if err != nil {
 			log.Error("invalid group_id")
-			ctx.JSON(http.StatusBadRequest, err.Error())
+			ctx.JSON(http.StatusBadRequest, gin.H{"code": 0, "data": "invalid group_id"})
 			return
 		}
-		perms = auth.PermissionGrantLabelsPairFromGroupIdAndPerm(groupId, auth.PermAdmin.Display)
+
+		perms := auth.PermissionGrantLabelsPairFromGroupIdAndPerm(groupId, auth.PermAdmin.Display)
+		for sk, sv := range stackBundle.Stack.Services {
+			if sv.Labels == nil {
+				sv.Labels = perms
+			} else {
+				for pk, pv := range perms {
+					sv.Labels[pk] = pv
+				}
+			}
+			stackBundle.Stack.Services[sk] = sv
+		}
 	}
 
-	if err := api.GetDockerClient().DeployStack(&stackBundle, perms); err != nil {
+	if err := api.GetDockerClient().DeployStack(&stackBundle); err != nil {
 		log.Error("Stack deploy got error: ", err)
 		ctx.JSON(http.StatusInternalServerError, err.Error())
 		return
