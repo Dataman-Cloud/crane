d31cafcd31103e19f681ea848fb0520fc1986757
my9074 <313231068@qq.com>
2016-07-29 11:19:27 +0800

    Add catalog list structure of file

--- src/registry/services/registry-backend.service.js
+++ src/registry/services/registry-backend.service.js
@@ -9,19 +9,29 @@
         return {
             listRepositories: listRepositories,
             listRepositoryTags: listRepositoryTags,
-            getImage: getImage
+            getImage: getImage,
+            listCatalogs: listCatalogs,
+            getCatalog: getCatalog
         };
-        
+
         function listRepositories() {
             return gHttp.Resource('registry.repositories').get();
         }
-        
+
         function listRepositoryTags(repository) {
             return gHttp.Resource('registry.listTags', {repository: repository}).get();
         }
-        
+
         function getImage(repository, tag) {
             return gHttp.Resource('registry.image', {repository: repository, tag: tag}).get();
+        }
+
+        function listCatalogs() {
+            return gHttp.Resource('registry.catalogs').get();
+        }
+
+        function getCatalog(catalogName) {
+            return gHttp.Resource('registry.catalog', {catalog_name: catalogName}).get();
         }
     }
 })();
