28a0abdf54e731df5cde9c84b8f1b3b57dceeddd
my9074 <313231068@qq.com>
2016-07-29 16:50:12 +0800

    Add createStack via catalog detail

--- src/registry/catalog-detail/detail.ctrl.js
+++ src/registry/catalog-detail/detail.ctrl.js
@@ -4,13 +4,70 @@
         .controller('CatalogDetailCtrl', CatalogDetailCtrl);
 
     /* @ngInject */
-    function CatalogDetailCtrl(catalog) {
+    function CatalogDetailCtrl(catalog, userBackend, $rootScope, stackCurd, $scope) {
         var self = this;
 
         self.catalog = catalog;
-        console.log(catalog)
+        self.stack = self.catalog.Bundle || "";
+        self.markdown = self.catalog.Readme;
 
+        self.stackName = '';
+        self.groups = [];
 
-        self.questions = angular.fromJson(catalog.Questions).questions;
+        self.form = {
+            Namespace: "",
+            Stack: ""
+        };
+
+        self.editorOptions = {
+            theme: 'midnight',
+            lineNumbers: true,
+            indentWithTabs: true,
+            matchBrackets: true,
+            mode: 'Javascript',
+            tabSize: 2,
+            extraKeys: {
+                Tab: function (cm) {
+                    var spaces = new Array(cm.getOption('indentUnit') + 1).join(' ');
+                    cm.replaceSelection(spaces);
+                }
+            }
+        };
+
+        self.errorInfo = {
+            stack: ''
+        };
+
+        self.loadGroups = loadGroups;
+        self.stackChange = stackChange;
+        self.create = create;
+
+        function loadGroups() {
+            userBackend.listGroup($rootScope.accountId)
+                .then(function (data) {
+                    self.groups = data
+                })
+        }
+
+        function stackChange() {
+            //clean error Info
+            self.errorInfo.stack = '';
+
+            stackValidate()
+        }
+
+        function stackValidate() {
+            try {
+                JSON.parse(self.stack)
+            } catch (err) {
+                self.errorInfo.stack = 'JSON 格式有误';
+            }
+        }
+
+        function create() {
+            self.form.Stack = angular.fromJson(self.stack);
+            stackCurd.createStack(self.form, $scope.staticForm, self.selectGroupId)
+        }
+
     }
 })();
