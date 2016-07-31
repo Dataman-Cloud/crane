2cc2d55fa8bac0a9484460c80fe1c56445c72a8e
Weitao Zhou <wtzhou@dataman-inc.com>
2016-07-31 18:06:20 -0400

    Dont merge me. test jenkins

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -2,7 +2,7 @@
 
 # maintainer weitao zhou <wtzhou@dataman-inc.com>
 
-docker run -i --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make
-docker run -i --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make collect-cover-data
-docker run -i --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make test-cover-html
-docker run -i --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make test-cover-func
+docker run --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make
+docker run --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make collect-cover-data
+docker run --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make test-cover-html
+docker run --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make test-cover-func
