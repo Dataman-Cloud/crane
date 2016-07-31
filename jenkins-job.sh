5afeb75d564dc85fbf42976e554793c7511b6e96
Weitao Zhou <wtzhou@dataman-inc.com>
2016-07-30 20:39:13 -0400

    Triger jenkins job

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# maintainer wtzhou
+
 docker run -it --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make
 docker run -it --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make collect-cover-data
 docker run -it --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make test-cover-html
