352527c5cf80d139b6c1af7901c66c00bf22f6ae
Zhou Weitao <zhouwtlord@gmail.com>
2016-08-02 14:52:35 +0800

    Wtzhou (#167)
    * Added test coverage func
    
    * Added jenkins-job.sh
    
    * Triger jenkins job
    
    * Fixed: cannot enable tty mode on non tty input
    
    * Test jenkins ci
    
    * Test Jenkins
    
    * Test Jenkins
    
    * CI

---
+++ jenkins-job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# maintainer weitao zhou <wtzhou@dataman-inc.com>
+
+docker run -i --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make
+docker run -i --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make collect-cover-data
+docker run -i --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make test-cover-html
+docker run -i --rm -w /go/src/github.com/Dataman-Cloud/rolex -v $(pwd):/go/src/github.com/Dataman-Cloud/rolex golang:1.5.4 make test-cover-func
