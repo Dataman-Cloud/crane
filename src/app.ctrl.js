ecccca7f32aa574676ebdab7c4be4e9396481d40
my9074 <313231068@qq.com>
2016-08-03 16:47:48 +0800

    Add global search bar in index.html

--- src/app.ctrl.js
+++ src/app.ctrl.js
@@ -4,7 +4,7 @@
         .controller('RootCtrl', RootCtrl);
 
     /* @ngInject */
-    function RootCtrl($state, $window, mdSideNav, gHttp, utils, userBackend, $rootScope, tty, stream) {
+    function RootCtrl($state, $window, mdSideNav, gHttp, utils, userBackend, $rootScope, tty, stream, layoutBackend) {
         var self = this;
 
         $rootScope.accountId = null;
@@ -13,8 +13,11 @@
         self.goBack = goBack;
         self.togShortMenu = togShortMenu;
         self.isShortMenu = false;
+        self.simulateQuery = true;
 
+        self.querySearch = querySearch;
         self.logout = logout;
+        self.searchJump = searchJump;
 
         activate();
 
@@ -29,13 +32,13 @@
                 gHttp.setToken(token);
                 tty.setToken(token);
                 stream.setToken(token);
-                
+
             } else {
                 utils.redirectLogin(true)
             }
 
             userBackend.aboutMe()
-                .then(function(data){
+                .then(function (data) {
                     $rootScope.accountId = data.Id
                 })
         }
@@ -57,6 +60,48 @@
 
         function togShortMenu() {
             self.isShortMenu = !self.isShortMenu;
+        }
+
+        function querySearch(query) {
+            return layoutBackend.globalSearch(query)
+        }
+
+        function searchJump(item) {
+            if (item) {
+                switch (item.Type) {
+                    case 'node':
+                        //go to node detail
+                        $state.go('node.detail', {node_id: item.Param.NodeId});
+                        break;
+                    case 'network':
+                        //go to network detail
+                        $state.go('node.networkDetail', {node_id: item.Param.NodeId, network_id: item.Param.NetworkID});
+                        break;
+                    case 'stack':
+                        //go to service list
+                        $state.go('stack.detail.service', {stack_name: item.Param.NameSpace});
+                        break;
+                    case 'service':
+                        //go to service detail
+                        $state.go('stack.serviceDetail', {
+                            stack_name: item.Param.NameSpace,
+                            service_id: item.Param.ServiceId
+                        });
+                        break;
+                    case 'task':
+                        //go to container detail
+                        $state.go('node.containerDetail', {
+                            node_id: item.Param.NodeId,
+                            container_id: item.Param.ContainerId
+                        });
+                        break;
+                    case 'volume':
+                        //go to volume list
+                        $state.go('node.detail.volume', {node_id: item.Param.NodeId});
+                        break;
+
+                }
+            }
         }
 
     }
