4b5f0b32534385f66a0eb099960b6724cf924416
kevin xu <cming.xu@gmail.com>
2016-08-09 18:46:52 +0800

    [MOD] catalog url change

--- src/plugins/registry/router.go
+++ src/plugins/registry/router.go
@@ -13,8 +13,8 @@ func (registry *Registry) RegisterApiForRegistry(router *gin.Engine, middlewares
 
 	registryV1Protected := router.Group("/registry/v1", middlewares...)
 	{
-		registryV1Protected.GET("/catalogs/mine", registry.MineCatalog)
-		registryV1Protected.GET("/catalogs/public", registry.PublicCatalog) // under library or tag marked as public
+		registryV1Protected.GET("/repositories/mine", registry.MineRepositories)
+		registryV1Protected.GET("/repositories/public", registry.PublicRepositories) // under library or tag marked as public
 		registryV1Protected.GET("/tag/list/:namespace/:image", registry.TagList)
 		registryV1Protected.GET("/manifests/:reference/:namespace/:image", registry.GetManifests)
 		registryV1Protected.PATCH("/:namespace/:image/publicity", registry.ImagePublicity)
