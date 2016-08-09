6e5ae2c5da11ee42bcb68a3807893788d51647cd
upccup <yaoyunsmile@gmial.com>
2016-08-09 13:47:05 -0400

    update swarmkit denpency

---
+++ vendor/github.com/docker/swarmkit/ca/testutils/cautils.go
@@ -0,0 +1,388 @@
+package testutils
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	cfcsr "github.com/cloudflare/cfssl/csr"
+	"github.com/cloudflare/cfssl/helpers"
+	"github.com/cloudflare/cfssl/initca"
+	"github.com/cloudflare/cfssl/log"
+	cfsigner "github.com/cloudflare/cfssl/signer"
+	"github.com/cloudflare/cfssl/signer/local"
+	"github.com/docker/distribution/digest"
+	"github.com/docker/swarmkit/api"
+	"github.com/docker/swarmkit/ca"
+	"github.com/docker/swarmkit/identity"
+	"github.com/docker/swarmkit/ioutils"
+	"github.com/docker/swarmkit/manager/state/store"
+	"github.com/docker/swarmkit/picker"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+// TestCA is a structure that encapsulates everything needed to test a CA Server
+type TestCA struct {
+	RootCA                ca.RootCA
+	ExternalSigningServer *ExternalSigningServer
+	MemoryStore           *store.MemoryStore
+	TempDir, Organization string
+	Paths                 *ca.SecurityConfigPaths
+	Server                grpc.Server
+	CAServer              *ca.Server
+	Context               context.Context
+	NodeCAClients         []api.NodeCAClient
+	CAClients             []api.CAClient
+	Conns                 []*grpc.ClientConn
+	Picker                *picker.Picker
+	WorkerToken           string
+	ManagerToken          string
+}
+
+// Stop cleansup after TestCA
+func (tc *TestCA) Stop() {
+	os.RemoveAll(tc.TempDir)
+	for _, conn := range tc.Conns {
+		conn.Close()
+	}
+	if tc.ExternalSigningServer != nil {
+		tc.ExternalSigningServer.Stop()
+	}
+	tc.CAServer.Stop()
+	tc.Server.Stop()
+}
+
+// NewNodeConfig returns security config for a new node, given a role
+func (tc *TestCA) NewNodeConfig(role string) (*ca.SecurityConfig, error) {
+	withNonSigningRoot := tc.ExternalSigningServer != nil
+	return genSecurityConfig(tc.MemoryStore, tc.RootCA, role, tc.Organization, tc.TempDir, withNonSigningRoot)
+}
+
+// WriteNewNodeConfig returns security config for a new node, given a role
+// saving the generated key and certificates to disk
+func (tc *TestCA) WriteNewNodeConfig(role string) (*ca.SecurityConfig, error) {
+	withNonSigningRoot := tc.ExternalSigningServer != nil
+	return genSecurityConfig(tc.MemoryStore, tc.RootCA, role, tc.Organization, tc.TempDir, withNonSigningRoot)
+}
+
+// NewNodeConfigOrg returns security config for a new node, given a role and an org
+func (tc *TestCA) NewNodeConfigOrg(role, org string) (*ca.SecurityConfig, error) {
+	withNonSigningRoot := tc.ExternalSigningServer != nil
+	return genSecurityConfig(tc.MemoryStore, tc.RootCA, role, org, tc.TempDir, withNonSigningRoot)
+}
+
+// WriteNewNodeConfigOrg returns security config for a new node, given a role and an org
+// saving the generated key and certificates to disk
+func (tc *TestCA) WriteNewNodeConfigOrg(role, org string) (*ca.SecurityConfig, error) {
+	withNonSigningRoot := tc.ExternalSigningServer != nil
+	return genSecurityConfig(tc.MemoryStore, tc.RootCA, role, org, tc.TempDir, withNonSigningRoot)
+}
+
+// External controls whether or not NewTestCA() will create a TestCA server
+// configured to use an external signer or not.
+var External bool
+
+// NewTestCA is a helper method that creates a TestCA and a bunch of default
+// connections and security configs.
+func NewTestCA(t *testing.T) *TestCA {
+	tempBaseDir, err := ioutil.TempDir("", "swarm-ca-test-")
+	assert.NoError(t, err)
+
+	s := store.NewMemoryStore(nil)
+
+	paths := ca.NewConfigPaths(tempBaseDir)
+	organization := identity.NewID()
+
+	rootCA, err := createAndWriteRootCA("swarm-test-CA", paths.RootCA, ca.DefaultNodeCertExpiration)
+	assert.NoError(t, err)
+
+	var (
+		externalSigningServer *ExternalSigningServer
+		externalCAs           []*api.ExternalCA
+	)
+
+	if External {
+		// Start the CA API server.
+		externalSigningServer, err = NewExternalSigningServer(rootCA, tempBaseDir)
+		assert.NoError(t, err)
+		externalCAs = []*api.ExternalCA{
+			{
+				Protocol: api.ExternalCA_CAProtocolCFSSL,
+				URL:      externalSigningServer.URL,
+			},
+		}
+	}
+
+	managerConfig, err := genSecurityConfig(s, rootCA, ca.ManagerRole, organization, "", External)
+	assert.NoError(t, err)
+
+	managerDiffOrgConfig, err := genSecurityConfig(s, rootCA, ca.ManagerRole, "swarm-test-org-2", "", External)
+	assert.NoError(t, err)
+
+	agentConfig, err := genSecurityConfig(s, rootCA, ca.AgentRole, organization, "", External)
+	assert.NoError(t, err)
+
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	assert.NoError(t, err)
+
+	baseOpts := []grpc.DialOption{grpc.WithTimeout(10 * time.Second)}
+	insecureClientOpts := append(baseOpts, grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{InsecureSkipVerify: true})))
+	clientOpts := append(baseOpts, grpc.WithTransportCredentials(agentConfig.ClientTLSCreds))
+	managerOpts := append(baseOpts, grpc.WithTransportCredentials(managerConfig.ClientTLSCreds))
+	managerDiffOrgOpts := append(baseOpts, grpc.WithTransportCredentials(managerDiffOrgConfig.ClientTLSCreds))
+
+	conn1, err := grpc.Dial(l.Addr().String(), insecureClientOpts...)
+	assert.NoError(t, err)
+
+	conn2, err := grpc.Dial(l.Addr().String(), clientOpts...)
+	assert.NoError(t, err)
+
+	conn3, err := grpc.Dial(l.Addr().String(), managerOpts...)
+	assert.NoError(t, err)
+
+	conn4, err := grpc.Dial(l.Addr().String(), managerDiffOrgOpts...)
+	assert.NoError(t, err)
+
+	serverOpts := []grpc.ServerOption{grpc.Creds(managerConfig.ServerTLSCreds)}
+	grpcServer := grpc.NewServer(serverOpts...)
+
+	managerToken := ca.GenerateJoinToken(&rootCA)
+	workerToken := ca.GenerateJoinToken(&rootCA)
+
+	createClusterObject(t, s, organization, workerToken, managerToken, externalCAs...)
+	caServer := ca.NewServer(s, managerConfig)
+	api.RegisterCAServer(grpcServer, caServer)
+	api.RegisterNodeCAServer(grpcServer, caServer)
+
+	ctx := context.Background()
+
+	go grpcServer.Serve(l)
+	go caServer.Run(ctx)
+
+	// Wait for caServer to be ready to serve
+	<-caServer.Ready()
+
+	remotes := picker.NewRemotes(api.Peer{Addr: l.Addr().String()})
+	picker := picker.NewPicker(remotes, l.Addr().String())
+
+	caClients := []api.CAClient{api.NewCAClient(conn1), api.NewCAClient(conn2), api.NewCAClient(conn3)}
+	nodeCAClients := []api.NodeCAClient{api.NewNodeCAClient(conn1), api.NewNodeCAClient(conn2), api.NewNodeCAClient(conn3), api.NewNodeCAClient(conn4)}
+	conns := []*grpc.ClientConn{conn1, conn2, conn3, conn4}
+
+	return &TestCA{
+		RootCA:                rootCA,
+		ExternalSigningServer: externalSigningServer,
+		MemoryStore:           s,
+		Picker:                picker,
+		TempDir:               tempBaseDir,
+		Organization:          organization,
+		Paths:                 paths,
+		Context:               ctx,
+		CAClients:             caClients,
+		NodeCAClients:         nodeCAClients,
+		Conns:                 conns,
+		CAServer:              caServer,
+		WorkerToken:           workerToken,
+		ManagerToken:          managerToken,
+	}
+}
+
+func createNode(s *store.MemoryStore, nodeID, role string, csr, cert []byte) error {
+	apiRole, _ := ca.FormatRole(role)
+
+	err := s.Update(func(tx store.Tx) error {
+		node := &api.Node{
+			ID: nodeID,
+			Certificate: api.Certificate{
+				CSR:  csr,
+				CN:   nodeID,
+				Role: apiRole,
+				Status: api.IssuanceStatus{
+					State: api.IssuanceStateIssued,
+				},
+				Certificate: cert,
+			},
+			Spec: api.NodeSpec{
+				Role:       apiRole,
+				Membership: api.NodeMembershipAccepted,
+			},
+		}
+
+		return store.CreateNode(tx, node)
+	})
+
+	return err
+}
+
+func genSecurityConfig(s *store.MemoryStore, rootCA ca.RootCA, role, org, tmpDir string, nonSigningRoot bool) (*ca.SecurityConfig, error) {
+	req := &cfcsr.CertificateRequest{
+		KeyRequest: cfcsr.NewBasicKeyRequest(),
+	}
+
+	csr, key, err := cfcsr.ParseRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Obtain a signed Certificate
+	nodeID := identity.NewID()
+	// All managers get added the subject-alt-name of CA, so they can be used for cert issuance
+	hosts := []string{role}
+	if role == ca.ManagerRole {
+		hosts = append(hosts, ca.CARole)
+	}
+
+	cert, err := rootCA.Signer.Sign(cfsigner.SignRequest{
+		Request: string(csr),
+		// OU is used for Authentication of the node type. The CN has the random
+		// node ID.
+		Subject: &cfsigner.Subject{CN: nodeID, Names: []cfcsr.Name{{OU: role, O: org}}},
+		// Adding ou as DNS alt name, so clients can connect to ManagerRole and CARole
+		Hosts: hosts,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Append the root CA Key to the certificate, to create a valid chain
+	certChain := append(cert, rootCA.Cert...)
+
+	// If we were instructed to persist the files
+	if tmpDir != "" {
+		paths := ca.NewConfigPaths(tmpDir)
+		if err := ioutil.WriteFile(paths.Node.Cert, certChain, 0644); err != nil {
+			return nil, err
+		}
+		if err := ioutil.WriteFile(paths.Node.Key, key, 0600); err != nil {
+			return nil, err
+		}
+	}
+
+	// Load a valid tls.Certificate from the chain and the key
+	nodeCert, err := tls.X509KeyPair(certChain, key)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeServerTLSCreds, err := rootCA.NewServerTLSCredentials(&nodeCert)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeClientTLSCreds, err := rootCA.NewClientTLSCredentials(&nodeCert, ca.ManagerRole)
+	if err != nil {
+		return nil, err
+	}
+
+	err = createNode(s, nodeID, role, csr, cert)
+	if err != nil {
+		return nil, err
+	}
+
+	if nonSigningRoot {
+		rootCA = ca.RootCA{
+			Cert:   rootCA.Cert,
+			Digest: rootCA.Digest,
+			Pool:   rootCA.Pool,
+		}
+	}
+
+	return ca.NewSecurityConfig(&rootCA, nodeClientTLSCreds, nodeServerTLSCreds), nil
+}
+
+func createClusterObject(t *testing.T, s *store.MemoryStore, clusterID, workerToken, managerToken string, externalCAs ...*api.ExternalCA) {
+	assert.NoError(t, s.Update(func(tx store.Tx) error {
+		store.CreateCluster(tx, &api.Cluster{
+			ID: clusterID,
+			Spec: api.ClusterSpec{
+				Annotations: api.Annotations{
+					Name: store.DefaultClusterName,
+				},
+				CAConfig: api.CAConfig{
+					ExternalCAs: externalCAs,
+				},
+			},
+			RootCA: api.RootCA{
+				JoinTokens: api.JoinTokens{
+					Worker:  workerToken,
+					Manager: managerToken,
+				},
+			},
+		})
+		return nil
+	}))
+}
+
+// createAndWriteRootCA creates a Certificate authority for a new Swarm Cluster.
+// We're copying ca.CreateAndWriteRootCA, so we can have smaller key-sizes for tests
+func createAndWriteRootCA(rootCN string, paths ca.CertPaths, expiry time.Duration) (ca.RootCA, error) {
+	// Create a simple CSR for the CA using the default CA validator and policy
+	req := cfcsr.CertificateRequest{
+		CN:         rootCN,
+		KeyRequest: cfcsr.NewBasicKeyRequest(),
+		CA:         &cfcsr.CAConfig{Expiry: ca.RootCAExpiration},
+	}
+
+	// Generate the CA and get the certificate and private key
+	cert, _, key, err := initca.New(&req)
+	if err != nil {
+		return ca.RootCA{}, err
+	}
+
+	// Convert the key given by initca to an object to create a ca.RootCA
+	parsedKey, err := helpers.ParsePrivateKeyPEM(key)
+	if err != nil {
+		log.Errorf("failed to parse private key: %v", err)
+		return ca.RootCA{}, err
+	}
+
+	// Convert the certificate into an object to create a ca.RootCA
+	parsedCert, err := helpers.ParseCertificatePEM(cert)
+	if err != nil {
+		return ca.RootCA{}, err
+	}
+
+	// Create a Signer out of the private key
+	signer, err := local.NewSigner(parsedKey, parsedCert, cfsigner.DefaultSigAlgo(parsedKey), ca.SigningPolicy(expiry))
+	if err != nil {
+		log.Errorf("failed to create signer: %v", err)
+		return ca.RootCA{}, err
+	}
+
+	// Ensure directory exists
+	err = os.MkdirAll(filepath.Dir(paths.Cert), 0755)
+	if err != nil {
+		return ca.RootCA{}, err
+	}
+
+	// Write the Private Key and Certificate to disk, using decent permissions
+	if err := ioutils.AtomicWriteFile(paths.Cert, cert, 0644); err != nil {
+		return ca.RootCA{}, err
+	}
+	if err := ioutils.AtomicWriteFile(paths.Key, key, 0600); err != nil {
+		return ca.RootCA{}, err
+	}
+
+	// Create a Pool with our Root CA Certificate
+	pool := x509.NewCertPool()
+	if !pool.AppendCertsFromPEM(cert) {
+		return ca.RootCA{}, fmt.Errorf("failed to append certificate to cert pool")
+	}
+
+	return ca.RootCA{
+		Signer: signer,
+		Key:    key,
+		Cert:   cert,
+		Pool:   pool,
+		Digest: digest.FromBytes(cert),
+	}, nil
+}
---
+++ vendor/github.com/docker/swarmkit/protobuf/plugin/raftproxy/raftproxy.go
@@ -0,0 +1,299 @@
+package raftproxy
+
+import (
+	"strings"
+
+	"github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
+	"github.com/gogo/protobuf/protoc-gen-gogo/generator"
+)
+
+const connErrorHandle = `
+	defer func() {
+		if err != nil {
+			errStr := err.Error()
+			if strings.Contains(errStr, grpc.ErrClientConnClosing.Error()) ||
+			strings.Contains(errStr, grpc.ErrClientConnTimeout.Error()) ||
+			strings.Contains(errStr, "connection error") ||
+			grpc.Code(err) == codes.Internal {
+				p.connSelector.Reset()
+			}
+		}
+	}()
+`
+
+type raftProxyGen struct {
+	gen *generator.Generator
+}
+
+func init() {
+	generator.RegisterPlugin(new(raftProxyGen))
+}
+
+func (g *raftProxyGen) Init(gen *generator.Generator) {
+	g.gen = gen
+}
+
+func (g *raftProxyGen) Name() string {
+	return "raftproxy"
+}
+
+func (g *raftProxyGen) genProxyStruct(s *descriptor.ServiceDescriptorProto) {
+	g.gen.P("type " + serviceTypeName(s) + " struct {")
+	g.gen.P("\tlocal " + s.GetName() + "Server")
+	g.gen.P("\tconnSelector raftpicker.Interface")
+	g.gen.P("\tcluster raftpicker.RaftCluster")
+	g.gen.P("\tctxMods []func(context.Context)(context.Context, error)")
+	g.gen.P("}")
+}
+
+func (g *raftProxyGen) genProxyConstructor(s *descriptor.ServiceDescriptorProto) {
+	g.gen.P("func NewRaftProxy" + s.GetName() + "Server(local " + s.GetName() + "Server, connSelector raftpicker.Interface, cluster raftpicker.RaftCluster, ctxMod func(context.Context)(context.Context, error)) " + s.GetName() + "Server {")
+	g.gen.P(`redirectChecker := func(ctx context.Context)(context.Context, error) {
+		s, ok := transport.StreamFromContext(ctx)
+		if !ok {
+			return ctx, grpc.Errorf(codes.InvalidArgument, "remote addr is not found in context")
+		}
+		addr := s.ServerTransport().RemoteAddr().String()
+		md, ok := metadata.FromContext(ctx)
+		if ok && len(md["redirect"]) != 0 {
+			return ctx, grpc.Errorf(codes.ResourceExhausted, "more than one redirect to leader from: %s", md["redirect"])
+		}
+		if !ok {
+			md = metadata.New(map[string]string{})
+		}
+		md["redirect"] = append(md["redirect"], addr)
+		return metadata.NewContext(ctx, md), nil
+	}
+	mods := []func(context.Context)(context.Context, error){redirectChecker}
+	mods = append(mods, ctxMod)
+	`)
+	g.gen.P("return &" + serviceTypeName(s) + `{
+		local: local,
+		cluster: cluster,
+		connSelector: connSelector,
+		ctxMods: mods,
+	}`)
+	g.gen.P("}")
+}
+
+func (g *raftProxyGen) genRunCtxMods(s *descriptor.ServiceDescriptorProto) {
+	g.gen.P("func (p *" + serviceTypeName(s) + `) runCtxMods(ctx context.Context) (context.Context, error) {
+	var err error
+	for _, mod := range p.ctxMods {
+		ctx, err = mod(ctx)
+		if err != nil {
+			return ctx, err
+		}
+	}
+	return ctx, nil
+}`)
+}
+
+func getInputTypeName(m *descriptor.MethodDescriptorProto) string {
+	parts := strings.Split(m.GetInputType(), ".")
+	return parts[len(parts)-1]
+}
+
+func getOutputTypeName(m *descriptor.MethodDescriptorProto) string {
+	parts := strings.Split(m.GetOutputType(), ".")
+	return parts[len(parts)-1]
+}
+
+func serviceTypeName(s *descriptor.ServiceDescriptorProto) string {
+	return "raftProxy" + s.GetName() + "Server"
+}
+
+func sigPrefix(s *descriptor.ServiceDescriptorProto, m *descriptor.MethodDescriptorProto) string {
+	return "func (p *" + serviceTypeName(s) + ") " + m.GetName() + "("
+}
+
+func (g *raftProxyGen) genClientStreamingMethod(s *descriptor.ServiceDescriptorProto, m *descriptor.MethodDescriptorProto) {
+	g.gen.P(sigPrefix(s, m) + "stream " + s.GetName() + "_" + m.GetName() + "Server) error {")
+	g.gen.P(`
+	if p.cluster.IsLeader() {
+			return p.local.` + m.GetName() + `(stream)
+	}
+	ctx, err := p.runCtxMods(stream.Context())
+	if err != nil {
+		return err
+	}
+	conn, err := p.connSelector.Conn()
+	if err != nil {
+		return err
+	}`)
+	g.gen.P(connErrorHandle)
+	g.gen.P("clientStream, err := New" + s.GetName() + "Client(conn)." + m.GetName() + "(ctx)")
+	g.gen.P(`
+	if err != nil {
+			return err
+	}`)
+	g.gen.P(`
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if err := clientStream.Send(msg); err != nil {
+			return err
+		}
+	}
+
+	reply, err := clientStream.CloseAndRecv()
+	if err != nil {
+		return err
+	}
+
+	return stream.SendAndClose(reply)`)
+	g.gen.P("}")
+}
+
+func (g *raftProxyGen) genServerStreamingMethod(s *descriptor.ServiceDescriptorProto, m *descriptor.MethodDescriptorProto) {
+	g.gen.P(sigPrefix(s, m) + "r *" + getInputTypeName(m) + ", stream " + s.GetName() + "_" + m.GetName() + "Server) error {")
+	g.gen.P(`
+	if p.cluster.IsLeader() {
+			return p.local.` + m.GetName() + `(r, stream)
+	}
+	ctx, err := p.runCtxMods(stream.Context())
+	if err != nil {
+		return err
+	}
+	conn, err := p.connSelector.Conn()
+	if err != nil {
+		return err
+	}`)
+	g.gen.P(connErrorHandle)
+	g.gen.P("clientStream, err := New" + s.GetName() + "Client(conn)." + m.GetName() + "(ctx, r)")
+	g.gen.P(`
+	if err != nil {
+			return err
+	}`)
+	g.gen.P(`
+	for {
+		msg, err := clientStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if err := stream.Send(msg); err != nil {
+			return err
+		}
+	}
+	return nil`)
+	g.gen.P("}")
+}
+
+func (g *raftProxyGen) genClientServerStreamingMethod(s *descriptor.ServiceDescriptorProto, m *descriptor.MethodDescriptorProto) {
+	g.gen.P(sigPrefix(s, m) + "stream " + s.GetName() + "_" + m.GetName() + "Server) error {")
+	g.gen.P(`
+	if p.cluster.IsLeader() {
+			return p.local.` + m.GetName() + `(stream)
+	}
+	ctx, err := p.runCtxMods(stream.Context())
+	if err != nil {
+		return err
+	}
+	conn, err := p.connSelector.Conn()
+	if err != nil {
+		return err
+	}`)
+	g.gen.P(connErrorHandle)
+	g.gen.P("clientStream, err := New" + s.GetName() + "Client(conn)." + m.GetName() + "(ctx)")
+	g.gen.P(`
+	if err != nil {
+			return err
+	}`)
+	g.gen.P(`errc := make(chan error, 1)
+	go func() {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			close(errc)
+			return
+		}
+		if err != nil {
+			errc <- err
+			return
+		}
+		if err := clientStream.Send(msg); err != nil {
+			errc <- err
+			return
+		}
+	}()`)
+	g.gen.P(`
+	for {
+		msg, err := clientStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if err := stream.Send(msg); err != nil {
+			return err
+		}
+	}
+	clientStream.CloseSend()
+	return <-errc`)
+	g.gen.P("}")
+}
+
+func (g *raftProxyGen) genSimpleMethod(s *descriptor.ServiceDescriptorProto, m *descriptor.MethodDescriptorProto) {
+	g.gen.P(sigPrefix(s, m) + "ctx context.Context, r *" + getInputTypeName(m) + ") (*" + getOutputTypeName(m) + ", error) {")
+	g.gen.P(`
+	if p.cluster.IsLeader() {
+			return p.local.` + m.GetName() + `(ctx, r)
+	}
+	ctx, err := p.runCtxMods(ctx)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := p.connSelector.Conn()
+	if err != nil {
+		return nil, err
+	}`)
+	g.gen.P(connErrorHandle)
+	g.gen.P("return New" + s.GetName() + "Client(conn)." + m.GetName() + "(ctx, r)")
+	g.gen.P("}")
+}
+
+func (g *raftProxyGen) genProxyMethod(s *descriptor.ServiceDescriptorProto, m *descriptor.MethodDescriptorProto) {
+	g.gen.P()
+	switch {
+	case m.GetServerStreaming() && m.GetClientStreaming():
+		g.genClientServerStreamingMethod(s, m)
+	case m.GetServerStreaming():
+		g.genServerStreamingMethod(s, m)
+	case m.GetClientStreaming():
+		g.genClientStreamingMethod(s, m)
+	default:
+		g.genSimpleMethod(s, m)
+	}
+	g.gen.P()
+}
+
+func (g *raftProxyGen) Generate(file *generator.FileDescriptor) {
+	g.gen.P()
+	for _, s := range file.Service {
+		g.genProxyStruct(s)
+		g.genProxyConstructor(s)
+		g.genRunCtxMods(s)
+		for _, m := range s.Method {
+			g.genProxyMethod(s, m)
+		}
+	}
+	g.gen.P()
+}
+
+func (g *raftProxyGen) GenerateImports(file *generator.FileDescriptor) {
+	if len(file.Service) == 0 {
+		return
+	}
+	g.gen.P("import raftpicker \"github.com/docker/swarmkit/manager/raftpicker\"")
+	g.gen.P("import codes \"google.golang.org/grpc/codes\"")
+	g.gen.P("import metadata \"google.golang.org/grpc/metadata\"")
+	g.gen.P("import transport \"google.golang.org/grpc/transport\"")
+}
