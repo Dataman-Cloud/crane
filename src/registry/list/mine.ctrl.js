fef02daeb4d9739af5f4b4577c72cdd1a3f38e37
kevin.xu <cming.xu@gmail.com>
2016-08-12 16:44:31 +0800

    [MOD] ui reginemnt done (#205)
    * [MOD] ui reginemnt done
    
    * [MOD] remove size with unit

--- src/registry/list/mine.ctrl.js
+++ src/registry/list/mine.ctrl.js
@@ -10,19 +10,23 @@
 
         self.repositories = repositories;
 
-        self.openRepository = openRepository;
-        self.closeRepository = closeRepository;
         self.deleteImage = deleteImage;
         self.publicImage = publicImage;
         self.hideImage = hideImage;
-        
+        self.toggleImagePublicity = toggleImagePublicity;
+
         activate()
-        
         function activate() {
             angular.forEach(self.repositories, function (repository) {
-              repository.tags = [];
-              repository.show = false;
               repository.name = repository.Namespace + "/" + repository.Image;
+              repository.tags = [];
+
+              registryBackend.listRepositoryTags(repository.name).then(function (data) {
+                  angular.forEach(data, function (tag) {
+                    repository.tags.push({tag: tag.Tag, digest: tag.Digest, size: tag.Size})
+                  });
+              });
+
             });
             angular.forEach(self.repositories, function (repository, index) {
                 if ($stateParams.open === repository.name) {
@@ -30,21 +34,15 @@
                 }
             })
         }
-        
-        function openRepository(index) {
-            registryBackend.listRepositoryTags(self.repositories[index].name).then(function (data) {
-                self.repositories[index].show = true;
-                self.repositories[index].tags = [];
-                angular.forEach(data, function (tag) {
-                  self.repositories[index].tags.push({tag: tag.Tag, digest: tag.Digest, size: tag.Size})
-                });
-            });
+
+        function toggleImagePublicity(repository) {
+          if(repository.Publicity == 1){
+            publicImage(repository.Namespace, repository.Image)
+          }else{
+            hideImage(repository.Namespace, repository.Image)
+          }
         }
-        
-        function closeRepository(index) {
-            self.repositories[index].show = false;
-        }
-        
+
         function deleteImage(repository, tag, ev) {
             registryCurd.deleteImage(repository, tag, ev);
         }
--- src/registry/list/public.ctrl.js
+++ src/registry/list/public.ctrl.js
@@ -8,42 +8,27 @@
     function RepositorieListPublicCtrl(repositories, registryBackend, registryCurd, utils, $rootScope, $stateParams) {
         var self = this;
 
+       self.repositories = repositories;
 
-       self.repositories = [];
-
-        self.openRepository = openRepository;
-        self.closeRepository = closeRepository;
-        
         activate()
-        
-        function activate() {
-            angular.forEach(repositories, function (repository) {
-                self.repositories.push({name: repository.Namespace + "/" + repository.Image,
-                                       pullCount: repository.PullCount,
-                                       pushCount: repository.PushCount,
-                                       tags: [],
-                                       show: false});
-            });
 
+        function activate() {
+            angular.forEach(self.repositories, function (repository) {
+              repository.name = repository.Namespace + "/" + repository.Image;
+              repository.tags = [];
+
+              registryBackend.listRepositoryTags(repository.name).then(function (data) {
+                  angular.forEach(data, function (tag) {
+                    repository.tags.push({tag: tag.Tag, digest: tag.Digest, size: tag.Size})
+                  });
+              });
+
+            });
             angular.forEach(self.repositories, function (repository, index) {
                 if ($stateParams.open === repository.name) {
                     openRepository(index);
                 }
             })
-        }
-        
-        function openRepository(index) {
-            registryBackend.listRepositoryTags(self.repositories[index].name).then(function (data) {
-                self.repositories[index].show = true;
-                self.repositories[index].tags = [];
-                angular.forEach(data, function (tag) {
-                  self.repositories[index].tags.push({tag: tag.Tag, digest: tag.Digest, size: tag.Size})
-                });
-            });
-        }
-        
-        function closeRepository(index) {
-            self.repositories[index].show = false;
         }
     }
 })();
