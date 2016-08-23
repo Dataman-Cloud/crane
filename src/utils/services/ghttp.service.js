2ab5eb9db3d263244d2f6ab1a047163ffa9d00a0
upccup <yaoyunsmile@163.com>
2016-08-23 17:58:55 +0800

    update error message

--- src/utils/services/ghttp.service.js
+++ src/utils/services/ghttp.service.js
@@ -122,9 +122,9 @@
 
             Resource.prototype._handleErrors = function (status, data, deferred) {
                 if (data && angular.isObject(data) && data.code && CODE_MESSAGE[data.code]) {
-                    if(SHOW_ERROR_DATA_MESSAGE.indexOf(data.code) !== -1){
-                        //notification data.data if SHOW_ERROR_MESSAGE includes code
-                        Notification.error(data.data);
+                    if(SHOW_ERROR_MESSAGE.indexOf(data.code) !== -1){
+                        //notification data.message if SHOW_ERROR_MESSAGE includes code
+                        Notification.error(data.message);
                     }else if(this.options.ignoreCodes.indexOf(data.code) === -1){
                         Notification.error(CODE_MESSAGE[data.code]);
                     }
