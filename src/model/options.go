09f4490d4f34946dbbe09e5f3838e3745f947e3e
kevin xu <cming.xu@gmail.com>
2016-08-23 14:04:45 +0800

    [MOD] list options moved to go-component

--- src/model/options.go
+++ src/model/options.go
@@ -2,13 +2,6 @@ package model
 
 import "encoding/json"
 
-type ListOptions struct {
-	Offset uint64
-	Limit  uint64
-
-	Filter map[string]interface{}
-}
-
 type UpdateOptions struct {
 	Method  string          `json:"Method"`
 	Options json.RawMessage `json:"Options"`
