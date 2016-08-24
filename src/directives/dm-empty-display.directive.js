861fab48bd498b6e8ec6ba679f0b011ac8975ffc
Myu <313231068@qq.com>
2016-08-24 16:40:47 +0800

    Update dmEmptyDisplay directive (#275)

--- src/directives/dm-empty-display.directive.js
+++ src/directives/dm-empty-display.directive.js
@@ -1,22 +1,37 @@
+/**
+ * @description
+ * if dm-empty-display value is invalid, the html and child html will replaced dm-empty-text value or '暂无信息'
+ * @example
+ <div flex="70" dm-empty-display="serviceConfigCtrl.service.Spec.Networks" dm-empty-text="未配置">
+     <ul>
+        <li data-ng-repeat="network in serviceConfigCtrl.service.Spec.Networks">{/network/}</li>
+     </ul>
+ </div>
+ */
+
 (function () {
     'use strict';
     angular.module('app')
         .directive('dmEmptyDisplay', dmEmptyDisplay);
 
     /* @ngInject */
-    function dmEmptyDisplay(utils) {
+    function dmEmptyDisplay(utils, $compile) {
         return {
             restrict: 'A',
             link: link,
             priority: 1001,
             scope: {
-                value: '=dmEmptyDisplay'
+                value: '=dmEmptyDisplay',
+                text: '@dmEmptyText'
             }
         };
 
         function link(scope, elem) {
+            var html = '<div class="no-info">{/text ? text : "暂无信息"/}</div>';
+
             if (utils.isEmpty(scope.value)) {
-                elem.replaceWith('<div class="no-info">暂无信息</div>');
+                var e = $compile(html)(scope);
+                elem.replaceWith(e);
             }
         }
     }
