02a4a334a76eb2dec4a3d7c9f8da1fadde191d21
kevin xu <cming.xu@gmail.com>
2016-08-26 09:49:59 +0800

    [MOD] minor change

--- src/stack/services/stack-curd.service.js
+++ src/stack/services/stack-curd.service.js
@@ -29,7 +29,7 @@
         }
 
         function deleteStack(ev, stackName) {
-            confirmModal.open("是否确认删除编排？", ev).then(function () {
+            confirmModal.open("应用删除后将无法恢复，确认要删除？", ev).then(function () {
                 stackBackend.deleteStack(stackName)
                     .then(function (data) {
                         $state.go('stack.list', undefined, {reload: true});
@@ -47,7 +47,7 @@
         function createStack(formData, form, groupId) {
             return stackBackend.createStack(formData, form, groupId)
                 .then(function (data) {
-                    Notification.success('创建成功');
+                    Notification.success('应用开始部署，部署时间依赖镜像拉取时间，请稍后');
                     $state.go('stack.detail.service', {stack_name: formData.Namespace})
                 })
         }
