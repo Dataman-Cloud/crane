b8861e49e99af70d0eb316b0b56103d4e9a35128
姚云 <yaoyunsmile@163.com>
2016-08-30 20:52:31 +0800

    add compress js command (#336)

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 REGISTRY_PREFIX=${REGISTRY_PREFIX:-demoregistry.dataman-inc.com/library/}
 VERSION=${VERSION:-v1.0.0rc5}
 
-docker run -it --rm -v $(pwd):/data digitallyseamless/nodejs-bower-grunt:5 bower install
+docker build -f ./deploy/compress.Dockerfile -t ${REGISTRY_PREFIX}compress:v1.0.0 .
+
+docker run -it --rm -v $(pwd):/data ${REGISTRY_PREFIX}compress:v1.0.0
 
 docker build -f ./deploy/Dockerfile -t ${REGISTRY_PREFIX}blackmamba:${VERSION} .
