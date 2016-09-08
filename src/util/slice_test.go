86a0aed77ed1e9fd1e0d0a577c8cf1c7e39c504a
yqguodataman <yqguo@dataman-inc.com>
2016-09-08 10:46:41 +0800

    update

--- src/util/slice_test.go
+++ src/util/slice_test.go
@@ -4,10 +4,18 @@ import (
 	"testing"
 )
 
-func TestStringInSlice(t *testing.T) {
+func TestSuccessStringInSlice(t *testing.T) {
 	if ok := StringInSlice("a", []string{"a", "b"}); ok {
 		t.Log("pass")
 	} else {
 		t.Error("faild")
+	}
+}
+
+func TestFaildStringInSlice(t *testing.T) {
+	if ok := StringInSlice("c", []string{"a", "b"}); ok {
+		t.Error("faild")
+	} else {
+		t.Log("pass")
 	}
 }
