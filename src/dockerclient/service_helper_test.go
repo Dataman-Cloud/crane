811b4907a43915bcfbdb62d4636385387f4bd308
yqguodataman <yqguo@dataman-inc.com>
2016-09-09 16:17:47 +0800

    add service helper test

--- src/dockerclient/service_helper_test.go
+++ src/dockerclient/service_helper_test.go
@@ -7,20 +7,21 @@ import (
 	"github.com/Dataman-Cloud/crane/src/dockerclient/model"
 
 	"github.com/docker/engine-api/types/swarm"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestValidateResources(t *testing.T) {
-	if err := validateResources(&swarm.Resources{}); err != nil {
-		t.Error("faild")
-	} else {
-		t.Log("pass")
-	}
+	err := validateResources(&swarm.Resources{})
+	assert.Nil(t, err)
 
-	if err := validateResources(&swarm.Resources{NanoCPUs: 1e6, MemoryBytes: 4 * 1024 * 1024}); err != nil {
-		t.Error("faild")
-	} else {
-		t.Log("pass")
-	}
+	err = validateResources(&swarm.Resources{NanoCPUs: 1e5})
+	assert.NotNil(t, err)
+
+	err = validateResources(&swarm.Resources{MemoryBytes: 3 * 1024 * 1024})
+	assert.NotNil(t, err)
+
+	err = validateResources(&swarm.Resources{NanoCPUs: 1e6, MemoryBytes: 4 * 1024 * 1024})
+	assert.Nil(t, err)
 }
 
 func TestValidateResourceRequirements(t *testing.T) {
@@ -120,4 +121,34 @@ func TestValidateImageName(t *testing.T) {
 	} else {
 		t.Log("pass")
 	}
+}
+
+func TestCheckPortConflicts(t *testing.T) {
+	reqPorts := map[string]bool{
+		"test": true,
+	}
+	service := swarm.Service{
+		ID: "test",
+		Spec: swarm.ServiceSpec{
+			EndpointSpec: &swarm.EndpointSpec{
+				Ports: []swarm.PortConfig{swarm.PortConfig{
+					TargetPort:    8080,
+					PublishedPort: 8080,
+				}},
+			},
+		},
+	}
+	if err := checkPortConflicts(reqPorts, "test", []swarm.Service{service}); err != nil {
+		t.Error(err)
+	} else {
+		t.Log("pass")
+	}
+}
+
+func TestPortConflictToString(t *testing.T) {
+	str := PortConflictToString(swarm.PortConfig{
+		Protocol:      swarm.PortConfigProtocolTCP,
+		PublishedPort: uint32(8080),
+	})
+	assert.Equal(t, str, "8080/tcp")
 }
