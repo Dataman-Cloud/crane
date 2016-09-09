9b2b14b2f84eb881cedc3dab984b0ec0c47f03fc
kevin xu <cming.xu@gmail.com>
2016-09-09 14:25:58 +0800

    [MOD] increase testing code coverage

---
+++ src/utils/httpresponse/httpresponse_test.go
@@ -0,0 +1,25 @@
+package httpresponse
+
+import (
+	"testing"
+
+	"github.com/Dataman-Cloud/crane/src/utils/cranerror"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestError(t *testing.T) {
+	assert.Nil(t, nil)
+}
+
+func TestExtractCraneError(t *testing.T) {
+	var err error
+	c, ok := extractCraneError(err)
+	assert.False(t, ok)
+	assert.Nil(t, c)
+
+	var cranerror cranerror.CraneError
+	cranerror.Code = "code"
+	c, ok = extractCraneError(&cranerror)
+	assert.True(t, ok)
+	assert.NotNil(t, c)
+}
