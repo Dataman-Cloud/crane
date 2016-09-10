f40dd0fb9e1a082cfc14b8a74648580c44c5a1b9
Ethan Chu <xychu2008@gmail.com>
2016-09-10 15:51:15 +0800

    Add test for listintercept middleware

---
+++ src/api/middlewares/listintercept_test.go
@@ -0,0 +1,33 @@
+package middlewares
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Dataman-Cloud/crane/src/utils/model"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestListIntercept(t *testing.T) {
+	var limit uint64
+	var offset uint64
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+	router.Use(ListIntercept())
+	router.GET("/listIntercept/test", func(c *gin.Context) {
+		listOptions := c.Value("listOptions").(model.ListOptions)
+		limit = uint64(listOptions.Limit)
+		offset = uint64(listOptions.Offset)
+		c.String(200, "OK")
+	})
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/listIntercept/test?page=1&per_page=10", nil)
+	router.ServeHTTP(w, r)
+
+	assert.Equal(t, uint64(1), limit)
+	assert.Equal(t, uint64(9), offset)
+}
