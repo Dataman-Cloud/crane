9970ae287c0e3a82064fdee1b90c79dd4b817e44
Zheng Liu <zliu@dataman-inc.com>
2016-09-10 13:37:54 +0800

    Add unit test for search/router.go

--- src/plugins/search/router_test.go
+++ src/plugins/search/router_test.go
@@ -3,6 +3,7 @@ package search
 import (
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -42,14 +43,16 @@ func TestIndices(t *testing.T) {
 	ds.Store = map[string]Document{
 		"document": testDocument,
 	}
-	indicesDS := ds.Indices()
-	assert.Equal(t, indicesDS, []string{"document"})
+	indices := ds.Indices()
+	keyList := make([]string, 1)
+	keyList[0] = "document"
+	assert.Equal(t, indices, keyList, "should be equal")
 }
 
 func TestSet(t *testing.T) {
 	ds := NewDocumentStorage()
-	ds.Set("test", testDocument)
-	assert.Equal(t, ds.Store["test"], testDocument)
+	ds.Set("document", testDocument)
+	assert.Equal(t, ds.Store["document"], testDocument, "should be equal")
 }
 
 func TestGet(t *testing.T) {
@@ -57,6 +60,30 @@ func TestGet(t *testing.T) {
 	ds.Store = map[string]Document{
 		"document": testDocument,
 	}
-	dc := ds.Get("document")
-	assert.Equal(t, dc, &testDocument)
+	value := ds.Get("document")
+	assert.Equal(t, value, &testDocument, "should be equal")
+}
+
+func TestRegisterApiForSearch(t *testing.T) {
+	router := gin.New()
+	searchApi := &SearchApi{}
+	searchApi.RegisterApiForSearch(router)
+	routes := router.Routes()
+	assert.Equal(t, len(routes), 1, "there should be one router")
+	if len(routes) == 1 {
+		route := routes[0]
+		assert.Equal(t, route.Method, "GET", "method should be get")
+		assert.Equal(t, route.Path, "/search/v1/luckysearch", "route path should be equal")
+		assert.Equal(t, route.Handler, "github.com/Dataman-Cloud/crane/src/plugins/search.(*SearchApi).Search-fm", "handler should be equal")
+	}
+
+}
+
+func TestIndexData(t *testing.T) {
+	searchApi := &SearchApi{}
+	searchApi.IndexData()
+	assert.Equal(t, len(searchApi.Index), 0, "should be equal")
+	assert.Empty(t, searchApi.Store, "should be equal")
+	assert.Empty(t, searchApi.PrefetchStore, "should be equal")
+	assert.Empty(t, searchApi.Indexer, "should be equal")
 }
