7cce27f84a03cb40954628cc0e0e2a6c481c8239
Weitao Zhou <wtzhou@dataman-inc.com>
2016-09-14 12:01:18 +0800

    Need not download the docker.tar if existed

--- bin/build-and-start.sh
+++ bin/build-and-start.sh
@@ -5,7 +5,10 @@ set -o errexit
 
 docker run --rm -v $(pwd)/frontend:/data digitallyseamless/nodejs-bower-grunt:5 bower install
 docker run --rm -w /go/src/github.com/Dataman-Cloud/crane -v $(pwd):/go/src/github.com/Dataman-Cloud/crane golang:1.5.4 make
-curl https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz | tar xzv
+
+if [ ! -f docker/docker ]; then
+    curl https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz | tar xzv
+fi
 
 export TAG=`git log --pretty=format:'%h' -n 1`
 export CRANE_SWARM_MANAGER_IP=$CRANE_IP
