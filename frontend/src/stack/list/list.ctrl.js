0bef4ea9466e020bfc0a66b982e7cc592b482bc5
lhfu <lhfu@dataman-inc.com>
2016-09-18 16:54:15 +0800

    Change stacks ip:port elem structure.

--- frontend/src/stack/list/list.ctrl.js
+++ frontend/src/stack/list/list.ctrl.js
@@ -12,8 +12,27 @@
 
         self.openCreateSelect = openCreateSelect;
 
+        activate();
+
+        function activate() {
+            formatStackServices(self.stacks);
+        }
+
         function openCreateSelect(ev) {
             stackCurd.createSelect(ev)
+        }
+
+        function formatStackServices(stacks) {
+            angular.forEach(stacks, function(stack) {
+                angular.forEach(stack.Services, function(service) {
+                    service.addrs = [];
+                    angular.forEach(service.IPs, function(ip) {
+                        angular.forEach(service.Ports, function(port) {
+                            service.addrs.push({ip: ip, port: port});
+                        });
+                    });
+                });
+            });
         }
     }
 })();
