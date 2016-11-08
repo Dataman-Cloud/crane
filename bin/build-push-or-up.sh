80432bc82e08d1fe8bfaaba8065ce7b2816fc7cb
Weitao Zhou <wtzhou@dataman-inc.com>
2016-11-08 12:48:46 +0800

    set defaulttag=latest

--- bin/build-push-or-up.sh
+++ bin/build-push-or-up.sh
@@ -6,7 +6,7 @@ export REGISTRY_PREFIX=${REGISTRY_PREFIX:-""}
 export DEFAULT_TAG=`git log --pretty=format:'%h' -n 1 2>/dev/null`
 if [ "x$DEFAULT_TAG" = "x" ]
 then
-    DEFAULT_TAG="emptytag"
+    DEFAULT_TAG="latest"
 fi
 export TAG=${VERSION:-$DEFAULT_TAG}
 
